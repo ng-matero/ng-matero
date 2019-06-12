ff9b17f5e7bc21332bb9a5f7d07f982301b37134
nzbin <nzb329@163.com>
2019-06-12 22:43:31 +0800

    refactor: fix sidemenu class issue

--- src/app/theme/admin-layout/admin-layout.component.ts
+++ src/app/theme/admin-layout/admin-layout.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss']
+  styleUrls: ['./admin-layout.component.scss'],
 })
 export class AdminLayoutComponent implements OnInit {
+  options = {
+    navigationPos: 'side',
+    sidebarStyle: 'full',
+    sidebarColor: 'white',
+    topbarColor: 'white',
+    dir: 'ltr',
+    useBreadcrumb: true,
+    topbarFixed: false,
+    breadcrumb: 'simple',
+    matTheme: 'egret-navy',
+    isMobile: false,
+    sidebarCompactToggle: false,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
