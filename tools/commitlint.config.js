90dd1acb2b1da1143be85848ee7b86d9e5ae4eab
nzbin <nzb329@163.com>
2019-09-24 21:47:10 +0800

    ci: modify pre-commit & formatting commitlint

--- tools/commitlint.config.js
+++ tools/commitlint.config.js
@@ -1,13 +1,24 @@
-"module.exports = {extends: ['@commitlint/config-conventional']}"
-
 module.exports = {
-	extends: ['@commitlint/config-conventional'],
-	rules: {
-			'type-enum': [2, 'always', [
-					"feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"
-			]],
-			'subject-full-stop': [0, 'never'],
-			'subject-case': [0, 'never']
-
-	}
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+  },
 };
