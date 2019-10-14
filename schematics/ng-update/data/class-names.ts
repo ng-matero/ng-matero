9e5b2a3224cdca95e974d636e2d1749a79d2145c
nzbin <nzb329@163.com>
2019-10-14 22:58:06 +0800

    refactor: add the frame of ng update

---
+++ schematics/ng-update/data/class-names.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import { ClassNameUpgradeData, TargetVersion, VersionChanges } from '@angular/cdk/schematics';
+
+export const classNames: VersionChanges<ClassNameUpgradeData> = {};
---
+++ schematics/ng-update/index.ts
@@ -0,0 +1,34 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import { Rule } from '@angular-devkit/schematics';
+import { createUpgradeRule, TargetVersion } from '@angular/cdk/schematics';
+import chalk from 'chalk';
+
+import { materialUpgradeData } from './upgrade-data';
+
+/** Entry point for the migration schematics with target of Ng-Matero v0 */
+export function updateToV0() {
+  console.log(chalk.green('The \`ng update\` is working!'));
+}
+
+/** Function that will be called when the migration completed. */
+function onMigrationComplete(targetVersion: TargetVersion, hasFailures: boolean) {
+  console.log();
+  console.log(chalk.green(`  ✓  Updated Ng-Matero to ${targetVersion}`));
+  console.log();
+
+  if (hasFailures) {
+    console.log(
+      chalk.yellow(
+        '  ⚠  Some issues were detected but could not be fixed automatically. Please check the ' +
+          'output above and fix these issues manually.'
+      )
+    );
+  }
+}
