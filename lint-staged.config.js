d640c3ca589fb867c33ffbd7e1eabae665fafa26
Arthur Ming <guobin.ming@pwc.com>
2020-02-17 22:26:16 +0800

    ci: add pre push git hooks

---
+++ lint-staged.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  '**/*.ts?(x)': filenames => (filenames.length > 10 ? 'ng lint' : `tslint --format verbose --project ./tsconfig.json --config ./tslint.json ${filenames.join(' ')}`),
+  '*.scss': filenames => `stylelint --syntax scss ${filenames.join(' ')}`,
+};
