8fb8d262ee31af8664f3cfaea133d705a4defbbd
nzbin <nzb329@163.com>
2020-02-23 11:29:42 +0800

    chore: format lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  '**/*.ts?(x)': filenames => (filenames.length > 10 ? 'ng lint' : `tslint --format verbose --project ./tsconfig.json --config ./tslint.json ${filenames.join(' ')}`),
+  '**/*.ts?(x)': filenames =>
+    filenames.length > 10
+      ? 'ng lint'
+      : `tslint --format verbose --project ./tsconfig.json --config ./tslint.json ${filenames.join(
+          ' '
+        )}`,
   '*.scss': filenames => `stylelint --syntax scss ${filenames.join(' ')}`,
 };
