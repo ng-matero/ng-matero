9b7c3e42a574037c61b4d9e6143dc04f3cf61664
nzbin <nzb329@163.com>
2020-03-09 23:54:51 +0800

    chore: update @ng-matero/extensions to 0.9.0

--- src/app/routes/tables/advanced/advanced.service.ts
+++ src/app/routes/tables/advanced/advanced.service.ts
@@ -18,69 +18,25 @@ export class TableAdvancedService {
       type: 'format',
       format: (data: any) => `<a href="${data.html_url}" target="_blank">${data.name}</a>`,
     },
-    {
-      title: 'Owner',
-      index: 'owner.login',
-    },
-    {
-      title: 'Owner Avatar',
-      index: 'owner.avatar_url',
-      type: 'img',
-    },
-    {
-      title: 'Description',
-      index: 'description',
-      width: '300px',
-    },
-    {
-      title: 'stars',
-      index: 'stargazers_count',
-    },
-    {
-      title: 'forks',
-      index: 'forks_count',
-    },
-    {
-      title: 'Score',
-      index: 'score',
-    },
-    {
-      title: 'Issues',
-      index: 'open_issues',
-    },
-    {
-      title: 'Language',
-      index: 'language',
-    },
-    {
-      title: 'License',
-      index: 'license.name',
-    },
-    {
-      title: 'Home Page',
-      index: 'homepage',
-      type: 'link',
-    },
+    { title: 'Owner', index: 'owner.login' },
+    { title: 'Owner Avatar', index: 'owner.avatar_url', type: 'img' },
+    { title: 'Description', index: 'description', width: '300px' },
+    { title: 'stars', index: 'stargazers_count' },
+    { title: 'forks', index: 'forks_count' },
+    { title: 'Score', index: 'score' },
+    { title: 'Issues', index: 'open_issues' },
+    { title: 'Language', index: 'language' },
+    { title: 'License', index: 'license.name' },
+    { title: 'Home Page', index: 'homepage', type: 'link' },
     {
       title: 'Is forked',
       index: 'fork',
       type: 'format',
       format: (data: any) => JSON.stringify(data.fork),
     },
-    {
-      title: 'Archived',
-      index: 'archived',
-      type: 'tag',
-      tag: TAG,
-    },
-    {
-      title: 'Created Date',
-      index: 'created_at',
-    },
-    {
-      title: 'Updated Date',
-      index: 'updated_at',
-    },
+    { title: 'Archived', index: 'archived', type: 'tag', tag: TAG },
+    { title: 'Created Date', index: 'created_at' },
+    { title: 'Updated Date', index: 'updated_at' },
   ];
 
   constructor(private http: HttpClient) {}
