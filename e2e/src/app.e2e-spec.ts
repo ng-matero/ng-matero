8ec0372a590c19177563bb2601f76d80d5fc9064
nzbin <nzb329@163.com>
2020-03-14 10:48:26 +0800

    refactor: update files to v9 and fix test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ng-test!');
+    expect(page.getTitleText()).toEqual('MATERO');
   });
 
   afterEach(async () => {
