865471cc4e11a243d6c76d9de3b351189577873e
nzbin <nzb329@163.com>
2020-08-01 10:10:15 +0800

    refactor: add disable control directive

---
+++ src/app/shared/directives/disable-control.directive.ts
@@ -0,0 +1,14 @@
+import { NgControl } from '@angular/forms';
+import { Directive, Input, SkipSelf, Optional } from '@angular/core';
+
+@Directive({
+  selector: '[disableControl]',
+})
+export class DisableControlDirective {
+  @Input() set disableControl(condition: boolean) {
+    const action = condition ? 'disable' : 'enable';
+    this.ngControl.control[action]();
+  }
+
+  constructor(@Optional() @SkipSelf() private ngControl: NgControl) {}
+}
