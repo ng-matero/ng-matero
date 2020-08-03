13e77f09fadeb9e1799b455028f2f6c902d08d17
nzbin <nzb329@163.com>
2020-08-03 08:10:39 +0800

    refactor: improve theme styles

--- src/app/theme/header/widgets/branding.component.ts
+++ src/app/theme/header/widgets/branding.component.ts
@@ -8,32 +8,5 @@ import { Component } from '@angular/core';
       <span class="matero-branding-name">MATERO</span>
     </a>
   `,
-  styles: [
-    `
-      .matero-branding {
-        display: inline-block;
-        color: inherit;
-        font-size: 16px;
-        white-space: nowrap;
-      }
-
-      .matero-branding-logo-expanded {
-        width: 30px;
-        height: 30px;
-        margin-right: 10px;
-        vertical-align: middle;
-
-        [dir='rtl'] & {
-          margin-left: 10px;
-          margin-right: auto;
-        }
-      }
-
-      .matero-branding-name {
-        font-weight: normal;
-        vertical-align: middle;
-      }
-    `,
-  ],
 })
 export class BrandingComponent {}
