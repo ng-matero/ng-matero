1c97a5f57356a7df70895249b323a5234171fd72
nzbin <nzb329@163.com>
2020-08-11 22:28:51 +0800

    chore: replace tsconfig.json to tsconfig.base.json

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   '**/*.ts?(x)': filenames =>
     filenames.length > 10
       ? 'ng lint'
-      : `tslint --format verbose --project ./tsconfig.json --config ./tslint.json ${filenames.join(
+      : `tslint --format verbose --project ./tsconfig.base.json --config ./tslint.json ${filenames.join(
           ' '
         )}`,
   'src/*.scss': filenames => `stylelint --syntax scss ${filenames.join(' ')}`,
