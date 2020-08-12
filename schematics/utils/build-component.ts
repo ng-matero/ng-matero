4ef7bbd384a21bd4c4d274d32ebacd180f19ba04
nzbin <nzb329@163.com>
2020-08-12 18:52:42 +0800

    chore(fix): fix schematics build error

--- schematics/utils/build-component.ts
+++ schematics/utils/build-component.ts
@@ -26,8 +26,8 @@ import { InsertChange } from '@schematics/angular/utility/change';
 import { getWorkspace } from '@schematics/angular/utility/config';
 import { buildRelativePath, findModuleFromOptions } from '@schematics/angular/utility/find-module';
 import { parseName } from '@schematics/angular/utility/parse-name';
-import { buildDefaultPath } from '@schematics/angular/utility/project';
 import { validateHtmlSelector, validateName } from '@schematics/angular/utility/validation';
+import { ProjectType, WorkspaceProject } from '@schematics/angular/utility/workspace-models';
 import { readFileSync, statSync } from 'fs';
 import { dirname, join, resolve } from 'path';
 import { getProjectFromWorkspace } from '@angular/cdk/schematics/utils/get-project';
@@ -36,6 +36,16 @@ import * as ts from '@schematics/angular/third_party/github.com/Microsoft/TypeSc
 
 export interface ComponentOptions extends Schema {
   pageName: string;
+}
+
+/**
+ * Build a default project path for generating.
+ * @param project The project to build the path for.
+ */
+function buildDefaultPath(project: WorkspaceProject): string {
+  const root = project.sourceRoot ? `/${project.sourceRoot}/` : `/${project.root}/src/`;
+  const projectDirName = project.projectType === ProjectType.Application ? 'app' : 'lib';
+  return `${root}${projectDirName}`;
 }
 
 /**
