5c24149934249fd653f95d4e45505c29508fcaa9
recca0120 <recca0120@gmail.com>
2020-08-17 19:10:18 +0800

    feat: get user from SettingsService

--- src/app/theme/sidebar/user-panel.component.ts
+++ src/app/theme/sidebar/user-panel.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
+import { SettingsService, User } from '@core';
 
 @Component({
   selector: 'app-user-panel',
   template: `
     <div class="matero-user-panel" fxLayout="column" fxLayoutAlign="center center">
-      <img
-        class="matero-user-panel-avatar"
-        src="assets/images/avatar.jpg"
-        alt="avatar"
-        width="64"
-      />
-      <h4 class="matero-user-panel-name">Zongbin</h4>
-      <h5 class="matero-user-panel-email">nzb329@163.com</h5>
+      <img class="matero-user-panel-avatar" [src]="user.avatar" alt="avatar" width="64" />
+      <h4 class="matero-user-panel-name">{{ user.name }}</h4>
+      <h5 class="matero-user-panel-email">{{ user.email }}</h5>
       <div class="matero-user-panel-icons">
         <a routerLink="/profile/overview" mat-icon-button>
           <mat-icon>account_circle</mat-icon>
@@ -27,4 +23,10 @@ import { Component } from '@angular/core';
   `,
   styleUrls: ['./user-panel.component.scss'],
 })
-export class UserPanelComponent {}
+export class UserPanelComponent {
+  user: User;
+
+  constructor(settings: SettingsService) {
+    this.user = settings.user;
+  }
+}
