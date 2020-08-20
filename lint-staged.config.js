276e9e24c70135b31fe6a71b10df2d9c5d6d39ae
Arthur Ming <guobin.ming@pwc.com>
2020-08-20 15:15:03 +0800

    ci: add prettier check in lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -6,4 +6,9 @@ module.exports = {
           ' '
         )}`,
   'src/*.scss': filenames => `stylelint --syntax scss ${filenames.join(' ')}`,
+  '*.{html,md,json,yml,js}': filenames => {
+    const prettier = `prettier --write -- ${filenames.join(' ')}`;
+    const git = `git add ${filenames.join(' ')}`;
+    return [prettier, git];
+  },
 };
