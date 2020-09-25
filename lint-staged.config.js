fce0bb851d4b5f8aeb77367665c6436a50c0b5bf
nzbin <nzb329@163.com>
2020-09-25 12:15:57 +0800

    build: remove prettier for style

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -7,8 +7,7 @@ module.exports = {
         )}`,
   'src/*.scss': filenames => `stylelint --syntax scss ${filenames.join(' ')}`,
   '*.{html,md,json,yml,js}': filenames => {
-    const prettier = `prettier --write -- ${filenames.join(' ')}`;
     const git = `git add ${filenames.join(' ')}`;
-    return [prettier, git];
+    return [git];
   },
 };
