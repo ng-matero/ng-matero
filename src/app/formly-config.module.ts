1c2485c2804959d446b818e992a450e5743bb4c1
nzbin <nzb329@163.com>
2020-11-10 22:16:35 +0800

    refactor(formly): add combobox template

--- src/app/formly-config.module.ts
+++ src/app/formly-config.module.ts
@@ -1,18 +1,23 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
+import { SharedModule } from './shared/shared.module';
+
 import { FormlyModule } from '@ngx-formly/core';
-import { FormlyValidation } from './formly-validation';
+import { FormlyFieldComboboxComponent } from './formly-template';
 import { FormlyWrapperPanelComponent } from './formly-wrapper';
+import { FormlyValidation } from './formly-validation';
 
 /**
  * Formly global configuration
  */
 const formlyModuleProviders = FormlyModule.forRoot({
+  types: [{ name: 'combobox', component: FormlyFieldComboboxComponent }],
   wrappers: [{ name: 'panel', component: FormlyWrapperPanelComponent }],
   validationMessages: [],
 }).providers;
 
 @NgModule({
-  declarations: [FormlyWrapperPanelComponent],
+  imports: [SharedModule],
+  declarations: [FormlyFieldComboboxComponent, FormlyWrapperPanelComponent],
   providers: [FormlyValidation],
 })
 export class FormlyConfigModule {
---
+++ src/app/formly-template.ts
@@ -0,0 +1,24 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FieldType } from '@ngx-formly/material/form-field';
+
+@Component({
+  selector: 'formly-field-combobox',
+  template: ` <mtx-select
+    [formControl]="formControl"
+    [items]="to.options | async"
+    [bindLabel]="to.labelProp"
+    [bindValue]="bindValue"
+    [multiple]="to.multiple"
+    [placeholder]="to.placeholder"
+    [required]="to.required"
+    [closeOnSelect]="!to.multiple"
+    [dropdownPosition]="'bottom'"
+  >
+  </mtx-select>`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FormlyFieldComboboxComponent extends FieldType {
+  get bindValue() {
+    return typeof this.to.valueProp === 'string' ? this.to.valueProp : undefined;
+  }
+}
