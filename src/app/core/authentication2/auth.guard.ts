b4ed724f1a16bf216cd02d653cb05c6161d10caf
recca0120 <recca0120@gmail.com>
2021-03-09 03:16:58 +0800

    feat: can activate child

--- src/app/core/authentication2/auth.guard.ts
+++ src/app/core/authentication2/auth.guard.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+  CanActivateChild,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '@core/authentication2/auth.service';
 import { tap } from 'rxjs/operators';
@@ -7,12 +14,20 @@ import { tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> {
+    return this.authenticate();
+  }
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authenticate();
+  }
+
+  private authenticate() {
     return this.authService.isAuthenticated().pipe(
       tap(authenticated => {
         if (!authenticated) {
@@ -21,5 +36,6 @@ export class AuthGuard implements CanActivate {
       }),
     );
   }
+
 
 }
