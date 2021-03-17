f1f4941a758a9601f8a1d2ebaa16dab0c32cd20b
recca0120 <recca0120@gmail.com>
2021-03-17 16:52:21 +0800

    test: add startup test

---
+++ src/app/core/bootstrap/startup.service.spec.ts
@@ -0,0 +1,59 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { StartupService } from './startup.service';
+import { TokenService } from '../authentication/token.service';
+import { LocalStorageService, MemoryStorageService } from '../../shared/services/storage.service';
+import { MenuService } from './menu.service';
+
+describe('StartupService', () => {
+  let httpMock: HttpTestingController;
+  let startup: StartupService;
+  let token: TokenService;
+  let menu: MenuService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: LocalStorageService,
+          useClass: MemoryStorageService,
+        },
+        StartupService,
+      ],
+    });
+    httpMock = TestBed.inject(HttpTestingController);
+    startup = TestBed.inject(StartupService);
+    token = TestBed.inject(TokenService);
+    menu = TestBed.inject(MenuService);
+  });
+
+  it('should load menu when token changed and token valid', () => {
+    const menuData = { menu: ['menu'] };
+    spyOn(menu, 'recursMenuForTranslation');
+    spyOn(menu, 'set');
+
+    startup.load();
+
+    token.set({ access_token: 'token' });
+
+    httpMock.expectOne('/me/menu').flush(menuData);
+
+    expect(menu.recursMenuForTranslation).toHaveBeenCalledWith(menuData.menu, 'menu');
+    expect(menu.set).toHaveBeenCalledWith(menuData.menu);
+  });
+
+  it('should clear menu when token changed and token invalid', () => {
+    spyOn(menu, 'recursMenuForTranslation');
+    spyOn(menu, 'set');
+
+    startup.load();
+
+    token.set({});
+
+    httpMock.expectNone('/me/menu');
+
+    expect(menu.recursMenuForTranslation).toHaveBeenCalledWith([], 'menu');
+    expect(menu.set).toHaveBeenCalledWith([]);
+  });
+});
--- src/app/core/bootstrap/startup.service.ts
+++ src/app/core/bootstrap/startup.service.ts
@@ -1,28 +1,27 @@
-import { Injectable, Injector } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { iif, of } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 import { MenuService } from './menu.service';
-import { AuthService } from '../authentication/auth.service';
 import { TokenService } from '@core/authentication/token.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StartupService {
+  private menu$ = this.http.get('/me/menu');
+
   constructor(private token: TokenService, private menu: MenuService, private http: HttpClient) {}
 
   load(): Promise<any> {
-    return new Promise(resolve => {
-      const menu$ = this.http.get('/me/menu');
-      this.token
-        .change()
-        .pipe(switchMap(() => iif(() => this.token.valid(), menu$, of({ menu: [] }))))
-        .subscribe((response: any) => {
-          this.menu.recursMenuForTranslation(response.menu, 'menu');
-          this.menu.set(response.menu);
-        });
-      resolve(null);
-    });
+    this.token
+      .change()
+      .pipe(switchMap(() => iif(() => this.token.valid(), this.menu$, of({ menu: [] }))))
+      .subscribe((response: any) => {
+        this.menu.recursMenuForTranslation(response.menu, 'menu');
+        this.menu.set(response.menu);
+      });
+
+    return Promise.resolve();
   }
 }
