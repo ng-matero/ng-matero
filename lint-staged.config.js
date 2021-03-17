31bb6eaba98cf6c80c77310a933384d43de628ef
nzbin <nzb329@163.com>
2021-03-17 20:53:26 +0800

    chore: add prettier in lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  '**/*.ts?(x)': filenames =>
-    filenames.length > 10
-      ? 'ng lint'
-      : `tslint --format verbose --project ./tsconfig.json --config ./tslint.json ${filenames.join(
-          ' '
-        )}`,
+  '**/*.ts?(x)': [
+    filenames =>
+      filenames.length > 10
+        ? 'ng lint'
+        : `tslint --format verbose --project ./tsconfig.json --config ./tslint.json ${filenames.join(
+            ' '
+          )}`,
+    'prettier --write',
+  ],
   'src/*.scss': filenames => `stylelint --syntax scss ${filenames.join(' ')}`,
   '*.{html,md,json,yml,js}': filenames => {
     const git = `git add ${filenames.join(' ')}`;
