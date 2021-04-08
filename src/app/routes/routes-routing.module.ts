c33ac747c0016b10d766d1d274a235cac7067c6d
nzbin <nzb329@163.com>
2021-04-08 14:27:08 +0800

    chore: remove unused data

--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -17,45 +17,34 @@ const routes: Routes = [
     canActivateChild: [AuthGuard],
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      {
-        path: 'dashboard',
-        component: DashboardComponent,
-        data: { title: 'Dashboard', titleI18n: 'dashboard' },
-      },
+      { path: 'dashboard', component: DashboardComponent },
       {
         path: 'design',
         loadChildren: () => import('./design/design.module').then(m => m.DesignModule),
-        data: { title: 'Design', titleI18n: 'design' },
       },
       {
         path: 'material',
         loadChildren: () => import('./material/material.module').then(m => m.MaterialModule),
-        data: { title: 'Material', titleI18n: 'material' },
       },
       {
         path: 'media',
         loadChildren: () => import('./media/media.module').then(m => m.MediaModule),
-        data: { title: 'Media', titleI18n: 'media' },
       },
       {
         path: 'forms',
         loadChildren: () => import('./forms/forms.module').then(m => m.FormsModule),
-        data: { title: 'Forms', titleI18n: 'forms' },
       },
       {
         path: 'tables',
         loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule),
-        data: { title: 'Tables', titleI18n: 'tables' },
       },
       {
         path: 'profile',
         loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule),
-        data: { title: 'Profile', titleI18n: 'profile' },
       },
       {
         path: 'sessions',
         loadChildren: () => import('./sessions/sessions.module').then(m => m.SessionsModule),
-        data: { title: 'Sessions', titleI18n: 'sessions' },
       },
       {
         path: 'helpers',
@@ -72,12 +61,8 @@ const routes: Routes = [
     path: 'auth',
     component: AuthLayoutComponent,
     children: [
-      { path: 'login', component: LoginComponent, data: { title: 'Login', titleI18n: 'login' } },
-      {
-        path: 'register',
-        component: RegisterComponent,
-        data: { title: 'Register', titleI18n: 'register' },
-      },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
     ],
   },
   { path: '**', redirectTo: 'dashboard' },
