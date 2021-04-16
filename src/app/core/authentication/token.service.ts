e112eb737ac7051d82f0b79193241dc321dd7eb2
recca0120 <recca0120@gmail.com>
2021-04-16 17:24:38 +0800

    refactor: remove refresh behavior subject and cleanup

--- src/app/core/authentication/token.service.ts
+++ src/app/core/authentication/token.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, of } from 'rxjs';
-import { delay, filter, map, share, switchMap } from 'rxjs/operators';
+import { BehaviorSubject, timer } from 'rxjs';
+import { filter, map, share, switchMap } from 'rxjs/operators';
 import { LocalStorageService } from '../../shared/services/storage.service';
 import { RefreshToken, Token } from './interface';
-
-function capitalize(str: string) {
-  return str.substring(0, 1).toUpperCase() + str.substring(1, str.length).toLowerCase();
-}
 
 @Injectable({
   providedIn: 'root',
@@ -14,15 +10,13 @@ function capitalize(str: string) {
 export class TokenService {
   private key = 'TOKEN';
   private change$ = new BehaviorSubject<RefreshToken>(this.get());
-  private refresh$ = new BehaviorSubject<RefreshToken>(this.get());
 
   constructor(private store: LocalStorageService) {}
 
   set(token: Token, refresh = false) {
-    token = this.updateExpiredAt(token);
-    this.store.set(this.key, token);
+    const refreshToken = this.toRefreshToken(token);
+    this.store.set(this.key, refreshToken);
     this.change$.next(Object.assign({ refresh }, token));
-    this.refresh$.next(token);
 
     return this;
   }
@@ -38,59 +32,56 @@ export class TokenService {
 
   change() {
     return this.change$.pipe(
-      filter(token => token.refresh !== true),
+      filter(token => !token.refresh),
       share()
     );
   }
 
   refresh() {
-    return this.refresh$.pipe(
-      map(token => token.expired_at),
-      filter(expiredAt => expiredAt > 0),
-      map(expiredAt => Math.max(0, expiredAt - this.now() - 5000)),
-      switchMap(delayTime => of(delayTime).pipe(delay(delayTime))),
+    return this.change$.pipe(
+      filter(token => token.expired_at > 0),
+      map(token => Math.max(0, token.expired_at - this.now() - 5000)),
+      switchMap(delay => timer(delay)),
       filter(() => this.valid()),
       share()
     );
   }
 
   valid() {
-    return !!this.getAccessToken() && !this.isExpired();
+    const token = this.get();
+
+    return !!token.access_token && !this.isExpired();
   }
 
   headerValue() {
-    const accessToken = this.getAccessToken();
+    const token = this.get();
 
-    return accessToken ? [this.getTokeType(), accessToken].join(' ') : '';
+    return !!token.access_token ? [token.token_type, token.access_token].join(' ') : '';
   }
 
   private isExpired() {
-    const expiredAt = this.getExpiredAt();
-
-    return expiredAt !== 0 && expiredAt - this.now() < 0;
-  }
-
-  private getAccessToken() {
     const token = this.get();
 
-    return token.access_token || token.token || '';
+    return token.expired_at !== 0 && token.expired_at - this.now() < 0;
   }
 
-  private getTokeType() {
-    return capitalize(this.get().token_type || 'bearer');
-  }
-
-  private getExpiredAt() {
-    return this.get().expired_at;
-  }
-
-  private updateExpiredAt(token: Token) {
-    return Object.assign(token, {
-      expired_at: !token.expires_in ? 0 : this.now() + token.expires_in * 1000,
-    });
+  private capitalize(str: string) {
+    return str.substring(0, 1).toUpperCase() + str.substring(1, str.length).toLowerCase();
   }
 
   private now() {
     return new Date().getTime();
+  }
+
+  private toRefreshToken(token: Token): RefreshToken {
+    const accessToken = token.access_token || token.token || '';
+    const tokenType = token.token_type || 'bearer';
+    const expiredIn = token.expires_in;
+
+    return Object.assign(token, {
+      access_token: accessToken,
+      token_type: this.capitalize(tokenType),
+      expired_at: !expiredIn ? 0 : this.now() + expiredIn * 1000,
+    });
   }
 }
