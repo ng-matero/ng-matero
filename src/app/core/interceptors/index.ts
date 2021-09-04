11b2818792d46077cf33f533fd5f166951ffe695
nzbin <nzb329@163.com>
2021-09-04 15:19:15 +0800

    fix(interceptor): fix intercept order issue

--- src/app/core/interceptors/index.ts
+++ src/app/core/interceptors/index.ts
@@ -3,10 +3,10 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NoopInterceptor } from './noop-interceptor';
 // import { SanctumInterceptor } from './sanctum-interceptor';
 import { BaseUrlInterceptor } from './base-url-interceptor';
-import { TokenInterceptor } from './token-interceptor';
 import { SettingsInterceptor } from './settings-interceptor';
-import { ErrorInterceptor } from './error-interceptor';
+import { TokenInterceptor } from './token-interceptor';
 import { DefaultInterceptor } from './default-interceptor';
+import { ErrorInterceptor } from './error-interceptor';
 import { LoggingInterceptor } from './logging-interceptor';
 
 /** Http interceptor providers in outside-in order */
@@ -14,9 +14,9 @@ export const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: NoopInterceptor, multi: true },
   // { provide: HTTP_INTERCEPTORS, useClass: SanctumInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: BaseUrlInterceptor, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: SettingsInterceptor, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: DefaultInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true },
 ];
