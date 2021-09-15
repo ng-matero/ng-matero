84a835ccf0e79c4d6f1536f741320acd43629cae
nzbin <nzb329@163.com>
2021-09-16 00:01:33 +0800

    chore: upgrade extensions to 12.4.0 and small fix

--- src/app/material-extensions.module.ts
+++ src/app/material-extensions.module.ts
@@ -4,14 +4,14 @@ import { MtxAlertModule } from '@ng-matero/extensions/alert';
 import { MtxButtonModule } from '@ng-matero/extensions/button';
 import { MtxCheckboxGroupModule } from '@ng-matero/extensions/checkbox-group';
 import { MtxColorPickerModule } from '@ng-matero/extensions/color-picker';
-import { MtxGridModule } from '@ng-matero/extensions/data-grid';
+import { MtxGridModule } from '@ng-matero/extensions/grid';
 import { MtxDatetimepickerModule } from '@ng-matero/extensions/datetimepicker';
 import { MtxDialogModule } from '@ng-matero/extensions/dialog';
 import { MtxLoaderModule } from '@ng-matero/extensions/loader';
 import { MtxPopoverModule } from '@ng-matero/extensions/popover';
 import { MtxProgressModule } from '@ng-matero/extensions/progress';
 import { MtxSelectModule } from '@ng-matero/extensions/select';
-import { MtxSplitModule } from '@ng-matero/extensions/split-pane';
+import { MtxSplitModule } from '@ng-matero/extensions/split';
 import { MtxTooltipModule } from '@ng-matero/extensions/tooltip';
 
 import { DatetimeAdapter, MTX_DATETIME_FORMATS } from '@ng-matero/extensions/core';
