21e5e351ba10587a16e17fab4412570ec9616f68
nzbin <nzb329@163.com>
2021-11-15 21:26:26 +0800

    chore: small fix

--- proxy.config.js
+++ proxy.config.js
@@ -6,16 +6,16 @@ const PROXY_CONFIG = {
     changeOrigin: true,
     secure: false,
     logLevel: 'debug',
-    onProxyReq: (proxyReq, req, res) => {
-      // const cookieMap = {
-      //   SID: '',
-      // };
-      // let cookie = '';
-      // for (const key of Object.keys(cookieMap)) {
-      //   cookie += `${key}=${cookieMap[key]}; `;
-      // }
-      // proxyReq.setHeader('cookie', cookie);
-    },
+    // onProxyReq: (proxyReq, req, res) => {
+    //   const cookieMap = {
+    //     SID: '',
+    //   };
+    //   let cookie = '';
+    //   for (const key of Object.keys(cookieMap)) {
+    //     cookie += `${key}=${cookieMap[key]}; `;
+    //   }
+    //   proxyReq.setHeader('cookie', cookie);
+    // },
   },
 };
 
