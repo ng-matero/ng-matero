a2ef4c2ab0be1beac10d3f6ca0a8e202c6a8bcad
nzbin <nzb329@163.com>
2021-12-17 13:34:30 +0800

    chore(schematics): remove chalk on update

--- schematics/ng-update/index.ts
+++ schematics/ng-update/index.ts
@@ -6,29 +6,25 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import { Rule } from '@angular-devkit/schematics';
+import { Rule, SchematicContext } from '@angular-devkit/schematics';
 import { createMigrationSchematicRule, TargetVersion } from '@angular/cdk/schematics';
-import * as chalk from 'chalk';
 
 import { materialUpgradeData } from './upgrade-data';
 
 /** Entry point for the migration schematics with target of Ng-Matero v0 */
-export function updateToV0() {
-  console.log(chalk.green('The `ng update` is working!'));
-}
+export function updateToV0() {}
 
 /** Function that will be called when the migration completed. */
-function onMigrationComplete(targetVersion: TargetVersion, hasFailures: boolean) {
-  console.log();
-  console.log(chalk.green(`  ✓  Updated Ng-Matero to ${targetVersion}`));
-  console.log();
+function onMigrationComplete(
+  context: SchematicContext,
+  targetVersion: TargetVersion,
+  hasFailures: boolean
+) {
+  context.logger.info('');
+  context.logger.info(`  ✓  Updated Ng-Matero to ${targetVersion}`);
+  context.logger.info('');
 
   if (hasFailures) {
-    console.log(
-      chalk.yellow(
-        '  ⚠  Some issues were detected but could not be fixed automatically. Please check the ' +
-          'output above and fix these issues manually.'
-      )
-    );
+    /** */
   }
 }
