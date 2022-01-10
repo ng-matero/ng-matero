f7ad8639ca9886a8b4adb69979ac2b9b66567a98
nzbin <nzb329@163.com>
2022-01-10 23:33:24 +0800

    chore(schematics): improve AppSettings types

--- schematics/ng-add/files/src/app/core/settings.ts
+++ schematics/ng-add/files/src/app/core/settings.ts
@@ -1,13 +1,13 @@
 export interface AppSettings {
-  navPos?: 'side' | 'top';
-  theme?: 'light' | 'dark';
-  dir?: 'ltr' | 'rtl';
-  showHeader?: boolean;
-  headerPos?: 'fixed' | 'static' | 'above';
-  showUserPanel?: boolean;
-  sidenavOpened?: boolean;
-  sidenavCollapsed?: boolean;
-  language?: string;
+  navPos: 'side' | 'top';
+  theme: 'light' | 'dark';
+  dir: 'ltr' | 'rtl';
+  showHeader: boolean;
+  headerPos: 'fixed' | 'static' | 'above';
+  showUserPanel: boolean;
+  sidenavOpened: boolean;
+  sidenavCollapsed: boolean;
+  language: string;
 }
 
 export const defaults: AppSettings = {
