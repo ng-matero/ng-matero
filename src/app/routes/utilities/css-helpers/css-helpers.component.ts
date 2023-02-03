aa040588f54626bb429cad0ab28d2ff31e2b49d1
nzbin <nzb329@163.com>
2023-02-03 22:47:23 +0800

    chore: update helpers docs

--- src/app/routes/utilities/css-helpers/css-helpers.component.ts
+++ src/app/routes/utilities/css-helpers/css-helpers.component.ts
@@ -10,4 +10,5 @@ export class UtilitiesCssHelpersComponent {
     'red|pink|purple|deep-purple|indigo|blue|light-blue|cyan|teal|green|ligh-green|lime|yellow|amber|orange|deep-orange|brown|gray|grey|blue-gray|blue-grey'.split(
       '|'
     );
+  colorHues = '50|100|200|300|400|500|600|700|800|900|A100|A200|A400|A700';
 }
