5512d36c8dfaf019f6c21f897eb11773427230dc
Arthur Ming <guobin.ming@pwc.com>
2023-04-16 09:40:37 +0800

    refactor: use `CanActivateFn` and `CanActivateChildFn` instead of `CanActivate` and `CanActivateChild`
    BREAKCHANGE: `AuthGuard` is removed`, please use `authenticate`
    
    close #500

--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -10,14 +10,14 @@ import { RegisterComponent } from './sessions/register/register.component';
 import { Error403Component } from './sessions/403.component';
 import { Error404Component } from './sessions/404.component';
 import { Error500Component } from './sessions/500.component';
-import { AuthGuard } from '@core/authentication';
+import { authenticate } from '@core/authentication';
 
 const routes: Routes = [
   {
     path: '',
     component: AdminLayoutComponent,
-    canActivate: [AuthGuard],
-    canActivateChild: [AuthGuard],
+    canActivate: [authenticate],
+    canActivateChild: [authenticate],
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: DashboardComponent },
