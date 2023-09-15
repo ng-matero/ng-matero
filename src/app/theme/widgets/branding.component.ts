2a5759f277e04c451dfb5292e00b5a2ede813436
nzbin <nzb329@163.com>
2023-09-15 14:36:29 +0800

    chore: update logo image

--- src/app/theme/widgets/branding.component.ts
+++ src/app/theme/widgets/branding.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-branding',
   template: `
     <a class="d-inline-block text-nowrap r-full text-reset" href="/">
-      <img src="./assets/images/matero.png" class="brand-logo align-middle m-2" alt="logo" />
+      <img src="./assets/images/matero.png" class="brand-logo align-middle m-2 r-full" alt="logo" />
       <span class="align-middle f-s-16 f-w-500 m-x-8">MATERO</span>
     </a>
   `,
