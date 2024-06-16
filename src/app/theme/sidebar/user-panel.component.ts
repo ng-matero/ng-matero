4c2f1cf5c64e36c5dcfb2aaf0b9243a91c6b9752
nzbin <nzb329@163.com>
2024-06-16 10:58:31 +0800

    feat: rebuild user panel with new style

--- src/app/theme/sidebar/user-panel.component.ts
+++ src/app/theme/sidebar/user-panel.component.ts
@@ -9,28 +9,11 @@ import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-user-panel',
   template: `
-    <div class="matero-user-panel">
+    <div class="matero-user-panel" routerLink="/profile/overview">
       <img class="matero-user-panel-avatar" [src]="user.avatar" alt="avatar" width="64" />
-      <h4 class="matero-user-panel-name">{{ user.name }}</h4>
-      <h5 class="matero-user-panel-email">{{ user.email }}</h5>
-      <div class="matero-user-panel-icons">
-        <button
-          mat-icon-button
-          routerLink="/profile/overview"
-          matTooltip="{{ 'profile' | translate }}"
-        >
-          <mat-icon>account_circle</mat-icon>
-        </button>
-        <button
-          mat-icon-button
-          routerLink="/profile/settings"
-          matTooltip="{{ 'edit_profile' | translate }}"
-        >
-          <mat-icon>edit</mat-icon>
-        </button>
-        <button mat-icon-button (click)="logout()" matTooltip="{{ 'logout' | translate }}">
-          <mat-icon>exit_to_app</mat-icon>
-        </button>
+      <div class="matero-user-panel-info">
+        <h4>{{ user.name }}</h4>
+        <h5>{{ user.email }}</h5>
       </div>
     </div>
   `,
